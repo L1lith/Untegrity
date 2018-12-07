5e95b8535bf28273989413cf1be04de67b30fa6d
L1lith <lily@l1lith.com>
2018-12-06 21:23:20 -0700

    improve video file detection

--- isVideoFile.js
+++ isVideoFile.js
@@ -1,10 +1,10 @@
 const {extname} = require('path')
-const knownVideoExtensions = [".webm",".mkv",".flv",".flv",".vob",".ogv",".ogg",".drc",".gif",".gifv",".mng",".avi",".MTS",".M2TS",".mov",".qt",".wmv",".yuv",".rm",".rmvb",".asf",".amv",".mp4",".m4p",".m4v",".mpg",".mp2",".mpeg",".mpe",".mpv",".mpg",".mpeg",".m2v",".m4v",".svi",".3gp",".3g2",".mxf",".roq",".nsv",".flv",".f4v",".f4p",".f4a",".f4b"]
+const knownVideoExtensions = [".webm",".mkv",".flv",".flv",".vob",".ogv",".ogg",".drc",".gif",".gifv",".mng",".avi",".mts",".m2ts",".mov",".qt",".wmv",".yuv",".rm",".rmvb",".asf",".amv",".mp4",".m4p",".m4v",".mpg",".mp2",".mpeg",".mpe",".mpv",".mpg",".mpeg",".m2v",".m4v",".svi",".3gp",".3g2",".mxf",".roq",".nsv",".flv",".f4v",".f4p",".f4a",".f4b"]
 
 function isVideoFile(path) {
   const extension = extname(path)
   if (!extension) return false
-  return knownVideoExtensions.includes(extension)
+  return knownVideoExtensions.includes(extension.toLowerCase())
 }
 
 module.exports = isVideoFile
