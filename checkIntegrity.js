a23727f1efc25e8b1809ccff9c128127c62629f1
L1lith <lily@l1lith.com>
2018-12-09 07:30:57 -0700

    add fatal ffmpeg error handling

--- checkIntegrity.js
+++ checkIntegrity.js
@@ -1,4 +1,4 @@
-const {exec} = require('child_process')
+const {spawn} = require('child_process')
 const {join} = require('path')
 const nanoid = require('nanoid')
 const mkdirpAsync = require('./mkdirpAsync')
@@ -18,22 +18,45 @@ async function checkIntegrity(videoPath, options) {
       await mkdirpAsync(logsDirectory)
       firstLaunch = false
     }
-    const errorLogPath = join(logsDirectory, nanoid() + '.log')
-    await new Promise((resolve, reject) => {
-      exec(`ffmpeg -v error -i "${videoPath}"${audioMode === true ? ' -map 0:1' : ''} -f null - >"${errorLogPath}" 2>&1`, ((err, stdout, stderr) => {
-        if (err) return reject(err)
-        if (stderr) console.log(stderr)
-        //if (stdout) console.log('stdout')
-        resolve()
-      }))
-    })
-    const errors = (await readFile(errorLogPath)).toString().split('\n').filter(line => line.trim().length > 0)
+    const errors = await runFFMPEGCheck(videoPath, {audioMode})
+    //console.log('hi', errors)
+    //await (new Promise(res => setTimeout(res, 100000)))
+    // await new Promise((resolve, reject) => {
+    //   exec(`ffmpeg -v error -i "${videoPath}"${audioMode === true ? ' -map 0:1' : ''} -f null - >"${errorLogPath}" 2>&1`, ((err, stdout, stderr) => {
+    //     if (err) return reject(err)
+    //     if (stderr) console.log(stderr)
+    //     //if (stdout) console.log('stdout')
+    //     resolve()
+    //   }))
+    // })
     const valid = errors.length === 0
-    await unlink(errorLogPath)
     if (returnErrors === true) {
       return errors
     }
     return valid
+}
+
+function runFFMPEGCheck(videoPath, options) {
+  return new Promise((resolve, reject) => {
+    const {audioMode=false} = options || {}
+    const errorLogPath = join(logsDirectory, nanoid() + '.log')
+    const process = spawn('ffmpeg', `-v error -i "${videoPath}"${audioMode === true ? ' -map 0:1' : ''} -f null - >"${errorLogPath}" 2>&1`.split(' '), {shell: true})
+    process.on('close', code => {
+      if (code === 0) {
+        readFile(errorLogPath).then(errorBuffer => {
+          const errors = errorBuffer.toString().split('\n').filter(line => line.trim().length > 0)
+          unlink(errorLogPath).then(()=>{
+            resolve(errors)
+          }).catch(reject)
+        }).catch(reject)
+      } else {
+        console.log("Fatal FFMPEG Error")
+        unlink(errorLogPath).then(()=>{
+          resolve(['Fatal FFMPEG Error'])
+        }).catch(reject)
+      }
+    })
+  })
 }
 
 module.exports = checkIntegrity
